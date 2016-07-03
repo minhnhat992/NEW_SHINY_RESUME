6e1bd78d4c0055558a21e81eb3449b38d5c53760
Nhat Bui <minhnhat992@gmail.com>
2016-07-03 17:42:27 -0400

    Add files via upload

--- app.R
+++ app.R
@@ -45,7 +45,7 @@ dbSide <- dashboardSidebar(
 dbBody <- dashboardBody(
   
   tabItems(
-    
+#Overview -------    
     tabItem(tabName = "Overview",
             
             h1("Brief Introduction"),
@@ -57,36 +57,84 @@ dbBody <- dashboardBody(
             h3("This is my self-created dasboard which highlights my best personal data science projects as well as my resume"),
             
             h3("Simply click on the tab menu to check out my work")),
-    
+# Resume -----    
     tabItem(tabName = "resume",
             
             HTML('<iframe src=\"https://drive.google.com/file/d/0B9pUvwmm7j8_U0F0R0swYzZPUlU/preview\" width=\"900\" height=\"900\"></iframe>')),
-    
+# Titanic -----     
     tabItem(tabName = "titanic",
             
             tabsetPanel(
               
-              tabPanel("Overview"),
+              tabPanel("Overview",
+                       
+                       includeMarkdown("www/Overview_Titanic_1.Rmd"),
+                       
+                       h4("Train Dataset"),
+                       
+                       DT::dataTableOutput("Train_Titan"),
+                       
+                       downloadButton("downloadTrainTitan", label = "Download"),
+                       
+                       h4("Test Dataset"),
+                       
+                       DT::dataTableOutput("Test_Titan"),
+                       
+                       downloadButton("downloadTestTitan", label = "Download")
+                       ),
               
-              tabPanel("EDA"),
+              tabPanel("EDA",
+                       
+                       includeMarkdown("www/EDA_Titanic_1.Rmd"),
+                       
+                       DT::dataTableOutput("Train_Titan_2"),
+                       
+                       includeMarkdown("www/EDA_Titanic_2.Rmd")),
               
-              tabPanel("Algorithm"),
+              tabPanel("Algorithm",
+                       
+                       htmlOutput("algorithm_titanic")),
               
-              tabPanel("Result"))),
+              tabPanel("Result",
+                       
+                       htmlOutput("result_titanic")))),
+
+# Insurance -----       
     tabItem(tabName = "insurance",
-            
+# Insucrance : Overview Tab ------            
             tabsetPanel(
-              tabPanel("Overview"),
+              tabPanel("Overview",
+                       includeMarkdown("www/Overview_1.Rmd"),
+                       
+                       h4("Train Dataset"),
+                       
+                       DT::dataTableOutput("Train"),
+                       
+                       downloadButton("downloadTrain", label = "Download"),
+                       
+                       h4("Test Dataset"),
+                       
+                       DT::dataTableOutput("Test"),
+                       
+                       downloadButton("downloadTest", label = "Download")
+                       
+                       ),
+# Insurance: EDA tab -------------              
+              tabPanel("EDA",
+                       
+                       htmlOutput("EDA_Insurance")),
               
-              tabPanel("EDA"),
+              tabPanel("Algorithm",
+                       
+                       htmlOutput("algo_Insu")),
               
-              tabPanel("Algorithm"),
-              
-              tabPanel("Result"))),
-    
+              tabPanel("Result",
+                       
+                       htmlOutput("result_Insu")))),
+# Scrape ---------------    
     tabItem(tabName = "movie",
             
-            withMathJax(includeMarkdown("www/Movie_Scraping.Rmd"))),
+            htmlOutput("movie_scraper")),
 # kobe tab -------
     tabItem(tabName = "kobe",
             
@@ -124,7 +172,24 @@ dbBody <- dashboardBody(
                            
                            mainPanel("Kobe's Short Chart based on selected year", 
                                      
-                                     plotOutput("shortchart", height = "800px" )))))))))
+                                     plotOutput("shortchart", height = "800px" ))))))),
+# API -------------    
+    tabItem(tabname = "api",
+            
+            tabsetPanel(
+              
+              tabPanel("Overview"),
+              
+              tabPanel("Twitter"),
+              
+              tabPanel("Steam"),
+              
+              tabPanel("Reddit"),
+              
+              tabPanel("Facebook")
+            ))
+
+))
 
   
 
@@ -165,8 +230,70 @@ server <- function(input, output){
   output$shortchart <-renderPlot({
     react()
   })
-
+  
+  getPage<-function() {
+    return(includeHTML("www/Movie_Scraping.html"))
   }
+  output$movie_scraper<-renderUI({getPage()})
+
+  output$Train <- DT::renderDataTable(trainning_set, server = TRUE, options=list(scrollX=TRUE))
+  
+  output$Test  <- DT::renderDataTable(test_set, server = TRUE, options=list(scrollX=TRUE))
+  
+  output$downloadTrain <- downloadHandler(filename = function(){"trainning_set.csv"},
+                                          
+                                          content = function(file){file.copy("www/trainning_set.txt",file)}) #work fine in browser, dont' worry
+  
+  output$downloadTest  <- downloadHandler(filename = function(){"test_set.csv"}, 
+                                          
+                                          content = function(file){file.copy("www/test_set.txt")})
+  InsuranceEDA <- function() {
+    return(includeHTML("www/EDA_Insurance.html"))
+  }
+  
+  output$EDA_Insurance <- renderUI({InsuranceEDA()})
+  
+  output$Train_Titan <- DT::renderDataTable(titan_train, server = TRUE, options=list(scrollX=TRUE))
+  
+  output$Test_Titan  <- DT::renderDataTable(titan_test, server = TRUE, options=list(scrollX=TRUE))
+  
+  output$downloadTrainTitan <- downloadHandler(filename = function(){"trainning_set.csv"},
+                                          
+                                          content = function(file){file.copy("www/Titan_train.csv",file)}) #work fine in browser, dont' worry
+  
+  output$downloadTestTitan  <- downloadHandler(filename = function(){"test_set.csv"}, 
+                                          
+                                          content = function(file){file.copy("www/Titan_test.csv")})
+  
+  output$Train_Titan_2 <- DT::renderDataTable(train, server = TRUE, options=list(scrollX=TRUE))
+  
+  algo_titan <- function() {
+    return(includeHTML("www/algorithm_Titanic.html"))
+  }
+  
+  output$algorithm_titanic <- renderUI({algo_titan()})
+  
+  result_titan <- function() {
+    return(includeHTML("www/result_Titanic.html"))
+  }
+  
+  output$result_titanic <- renderUI({result_titan()})
+  
+  algo_insurance <- function() {
+    return(includeHTML("www/algorithm.html"))
+  }
+  
+  output$algo_Insu <- renderUI({algo_insurance()})
+  
+  result_insurance <- function() {
+    return(includeHTML("www/result_Insurance.html"))
+  }
+  
+  output$result_Insu <- renderUI({result_insurance()})
+    
+}
+  
+
 
 #Shiny app -----
 
--- load.R
+++ load.R
@@ -13,6 +13,24 @@ library(ggplot2)
 library(grid)
 library(jpeg)
 library(RCurl)
+library(DT)
+library(doParallel)
+library(stepPlr)
+library(pROC)
+library(ROCR)
+library(psych)
+library(ROSE)
+library(caret)
+library(Hmisc)
+library(plyr)
+library(dplyr)# for %>% 
+library(data.table)
+library(sqldf)
+library(gdata)
+library(utils)#read csv
+library(car)# for multicollinearity
+library(MASS)#chi-square test of independence
+
 
 load("raw.rda")
 
@@ -21,6 +39,16 @@ court_img <- "http://robslink.com/SAS/democd54/nba_court_dimensions.jpg"
 court <- rasterGrob(readJPEG(getURLContent(court_img)),
                     width=unit(1,"npc"), height=unit(1,"npc"))
 
+trainning_set <- read.csv("www/trainning_set.txt")
+test_set <- read.csv("www/test_set.txt")
+titan_train <- read.csv("www/Titan_test.csv")
+titan_test <- read.csv("www/Titan_train.csv")
 
 
+load(file = "www/actor_final.rda")
+load(file = "www/important_people.rda")
+load(file = "www/first_row.rda")
+load(file = "www/xmltop1.rda")
+load(file = "www/xmlfile1.rda")
+load(file = "www/train_titanic.rda")
 
